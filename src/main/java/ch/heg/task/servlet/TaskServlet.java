8b0cba1819dd1d812b8d877f609644749b619c4f
Frederic Mencier <fmencier@yahoo.fr>
2024-05-14 21:26:37 +0200

    work

--- src/main/java/ch/heg/task/servlet/TaskServlet.java
+++ src/main/java/ch/heg/task/servlet/TaskServlet.java
@@ -1,5 +1,6 @@
 package ch.heg.task.servlet;
 
+import ch.heg.task.entities.Statut;
 import ch.heg.task.entities.Task;
 import ch.heg.task.services.TachesServices;
 import jakarta.servlet.annotation.WebServlet;
@@ -10,8 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
-@WebServlet(urlPatterns = "/task/*")
+@WebServlet(urlPatterns = "/tasks/*")
 public class TaskServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
@@ -19,20 +21,36 @@ public class TaskServlet extends HttpServlet {
     TachesServices tachesServices;
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        doGet(request,response);
+        doGet(request, response);
     }
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
         String requestURI = request.getRequestURI();
         String htmlResponse = "";
-        if (requestURI.equals("/task/search")) {
+        if (requestURI.equals("/tasks/search")) {
             String id = request.getParameter("id");
             if (id != null) {
                 htmlResponse = getSearchResponse(id);
             } else {
                 htmlResponse = "id is null";
             }
+        } else if (requestURI.equals("/tasks")) {
+            String desc = request.getParameter("desc");
+            String mail = request.getParameter("mail");
+            if (desc != null && mail != null) {
+                Task task = new Task();
+                task.setDescription(desc);
+                task.setEmailResponsable(mail);
+                task.setDateCreation(new Date());
+                task.setStatut(Statut.TODO);
+                Task taskCreated = tachesServices.addTask(task);
+                htmlResponse = "Task Created with id : %s".formatted(taskCreated.getId());
+            } else {
+                htmlResponse = "bad parameters";
+            }
+        } else {
+            htmlResponse = "URI not available";
         }
+        htmlResponse = htmlResponse + "<br><br><a href=\"http://localhost:8081/index.html\">http://localhost:8081/index.html</a>";
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         out.println(htmlResponse);
